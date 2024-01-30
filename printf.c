c5c0f0ede744bebc72d7440aaa96dd57ceaa44c7
lilynjeri <liliann.mwangi@gmail.com>
2024-01-30 11:27:48 -0800

    My task 0, printf.c commit

--- printf.c
+++ printf.c
@@ -1,50 +1,68 @@
 #include "main.h"
-
 /**
- * _printf - Acts like the printf function in the standard input and output
- * @format: Takes a string that needs to be printed
- * @...: A varidic function that takes integers, strings, characters etc
- * Return:  chara_points
+ * _printf -acts like the printf function in the standard input and output
+ * @format: takes a string that needs to be printed
+ * @...: a varidic function that takes integers, strings, characters etc
+ * Return: chara_points
  */
-
 int _printf(const char *format, ...)
 {
-	va_list list_1;
-	char *chara_points = NULL
-		;
-	va_start(list_1, format);
+	int chara_points = 0;
 
-	while (*format)
+	va_list list_1;
+
+	va_start(list_1, format);
+	if (format == NULL)
 	{
-		if (*format == '%' && *(format + 1) != '\0')
-		{
-			switch (*(format + 1))
-			{
-				case 'c':
-					chara_points = va_arg(list_1, char *);
-					write(1, chara_points, 1);
-					break;
-				case 's':
-					chara_points = va_arg(list_1, char *);
-					write(1, chara_points, strlen(chara_points));
-					break;
-				case '%':
-					write(1, "%", 1);
-					break;
-				default:
-					write(1, "%", 1);
-					write(1, format + 1, 1);
-					break;
-			}
-			format += 2;
-		}
+		return (-1);
+	}
+	while (*format != '\0')
+	{
+		if (*format != '%')
+			write(1, format, 1), chara_points++;
 		else
 		{
-			write(1, format, 1);
 			format++;
-		}
-	}
+			if (*format == '\0')
+				break;
 
+			switch (*format)
+			{
+				case 'c':
+				{
+					char alpha = va_arg(list_1, int);
+
+					write(1, &alpha, 1), chara_points++;
+					break;
+				}
+				case 's':
+				{
+					char *strings = va_arg(list_1, char*);
+
+					while (*strings)
+						write(1, strings++, 1), chara_points++;
+					break;
+				}
+				case 'd':
+				case 'i':
+				{
+					int num = va_arg(list_1, int);
+
+					char buffer[12];
+
+					int length = sprintf(buffer, "%d", num);
+
+					write(1, buffer, length), chara_points += length;
+
+					break;
+				}
+				case '%':
+					write(1, format, 1), chara_points++;
+					break;
+			}
+		}
+		format++;
+	}
 	va_end(list_1);
-	return (0);
+	return (chara_points);
 }
